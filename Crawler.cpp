35eb92a762b2d70cddecab201ba293ae2c2839bb
DarrenMeidl <dmeidl15@gmail.com>
2024-04-02 14:43:31 +0100

    Finished Crawler, checks if it can move, moves+1, adds pair to list

--- Crawler.cpp
+++ Crawler.cpp
@@ -1,21 +1,43 @@
 //
 // Created by dmeid on 02/04/2024.
 //
-
+#include <random>
 #include "Crawler.h"
+
+
+std::random_device rd; // Create random number generator called rd
+std::mt19937 gen(rd()); // Seed the generator with a random device
+
+
 void Crawler::move() {
-    // Move +1 unit in the direction facing
-    // 1 = up, 2 = right, 3 = down, 4 = lef
-    if (direction == 1){ // if facing north
-        position.second++; // increment the y integer in the pair by 1 i.e. move +1 upwards while keeping x the same
+    int tries = 0;
+    const int max = 100; // max tries to find a valid move direction
+    // while we are blocked at the edge of the board
+    while (isWayBlocked() && tries < max){
+        std::uniform_int_distribution<> dis(1, 4); // declare random integer between 1-4
+        direction = dis(gen); //set direction to that random integer
+        tries++; // increment attempts
     }
-    else if (direction == 2){ // right
-        position.first++;
+    // If we're not blocked
+    if (!isWayBlocked()){
+        // Move +1 unit in the direction facing
+        // 1 = up, 2 = right, 3 = down, 4 = left
+        if (direction == 1){ // if facing north
+            position.second++; // increment the y integer in the pair by 1 i.e. move +1 upwards while keeping x the same
+        }
+        else if (direction == 2){ // move right
+            position.first++;
+        }
+        else if (direction == 3){ // move down
+            position.second--;
+        }
+        else if (direction == 4){ // move left
+            position.first--;
+        }
+    } else{ // print an error if the crawler can't move after 100 tries
+        std::cout << "Crawler " << id << " couldn't find a valid direction, staying put." << std::endl;
     }
-    else if (direction == 3){ // down
-        position.second--;
-    }
-    else if (direction == 4){ // left
-        position.first--;
-    }
+    // if crawler couldn't find a way to move, then it simply stays in the same position
+    // add current position as a pair to the path list (emplace_back is cleaner looking than "push_back(std::make_pair...."
+    path.emplace_back(position.first, position.second);
 }
