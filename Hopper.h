799b41f225bb546ba07490c0c7407f35bc9f6405
DarrenMeidl <dmeidl15@gmail.com>
2024-04-02 15:10:30 +0100

    Created Hopper class

---
+++ Hopper.h
@@ -0,0 +1,15 @@
+//
+// Created by dmeid on 02/04/2024.
+//
+
+#ifndef MEIDL_DARREN_CA2_HOPPER_H
+#define MEIDL_DARREN_CA2_HOPPER_H
+#include "Bug.h" // ensures this derived class properly inherits base class
+
+class Hopper : protected Bug {
+public: // allows the function to be accessible from outside the class hierarchy
+
+};
+
+
+#endif //MEIDL_DARREN_CA2_HOPPER_H
