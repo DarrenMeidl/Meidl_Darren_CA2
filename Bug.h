8dd83cdbce7b688987e9547b264bf4074b85969d
DarrenMeidl <dmeidl15@gmail.com>
2024-04-02 13:13:25 +0100

    filled in .h file for Bug class

--- Bug.h
+++ Bug.h
@@ -1,13 +1,23 @@
 //
 // Created by dmeid on 02/04/2024.
 //
-
+#include <iostream>
+#include <utility>
+#include <list>
 #ifndef MEIDL_DARREN_CA2_BUG_H
 #define MEIDL_DARREN_CA2_BUG_H
 
 
 class Bug {
-
+    int id;
+    std::pair<int, int> position;
+    int direction;
+    int size;
+    bool alive;
+    std::list<std::pair<int, int>> path;
+public:
+    virtual void move();
+    bool isWayBlocked();
 };
 
 
