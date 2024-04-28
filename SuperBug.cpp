00c84579ddee00ee18d2f67bc5b8db829285b4a8
DarrenMeidl <dmeidl15@gmail.com>
2024-04-28 10:03:42 +0100

    Added wayblock check for SuperBug

--- SuperBug.cpp
+++ SuperBug.cpp
@@ -8,16 +8,24 @@ void SuperBug::move() {
     if (alive){ // don't move if we are dead
         // Move the SuperBug based on arrow key input
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            position.second++;
+            direction = 1;
+            if (!isWayBlocked())
+                position.second++;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            position.second--;
+            direction = 3;
+            if (!isWayBlocked())
+                position.second--;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            position.first--;
+            direction = 4;
+            if (!isWayBlocked())
+                position.first--;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            position.first++;
+            direction = 2;
+            if (!isWayBlocked())
+                position.first++;
         }
         // add current position as a pair to the path list (emplace_back is cleaner looking than "push_back(std::make_pair...."
         path.emplace_back(position.first, position.second);
